cbec214a8a34b914706b0a86412366c824045187
Guilherme oliveira <161101698+okguilherme@users.noreply.github.com>
2024-05-09 16:30:19 -0300

    código
    player

--- cenas/entidades/jogador/player.gd
+++ cenas/entidades/jogador/player.gd
@@ -1,28 +1,33 @@
 extends CharacterBody2D
 
+const  SPEED = 300.0
+var JUMP_force = -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var booleano = 0
+var botao_pressionado 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_force
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	var num = Input.is_action_pressed("corre rapido")
+	print (num)
+	
+	if num:
+		velo *= 2
+
+
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * velo
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, velo)
 
 	move_and_slide()
+	
