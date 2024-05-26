6f9985464c9637b77f0ba32bd4eb09e9d935ea1a
Guilherme oliveira <161101698+okguilherme@users.noreply.github.com>
2024-05-25 23:03:45 -0400

    código

---
+++ scripts/sistemas/intro/intro.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _ready():
+	Transicao.scene_path = "res://cenas/sistemas/sistemas/menu.tscn"
+	Transicao.fade_in()
---
+++ scripts/sistemas/transicao/transicao.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@onready var animation: AnimationPlayer = get_node("AnimationPlayer") 
+
+var scene_path: String = ""
+
+func fade_in() -> void:
+	animation.play("fade_in")
+
+	
+	
+
+
+func _on_animation_player_animation_finished(anim_name: String) -> void:
+	if anim_name == "fade_in":
+		get_tree().change_scene_to_file(scene_path)
+		animation.play("fade_out")
